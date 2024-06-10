882e6df125e2ae376f4dda9486ba4b130e8c3a0b
Ilia Rakhoutine <08rakhoutine@gmail.com>
2024-06-10 12:23:13 -0400

    created class with constructor

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -1,1 +1,23 @@
-s
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+
+class Person {
+private:
+    int age;
+    string name;
+    string gender;
+    double weight;
+
+public:
+    Person(int age, string name, string gender, double weight)
+        : age(age), name(name), weight(weight) {
+            if (gender == "male" || gender == "female") {
+                this->gender = gender;
+            } else {
+                throw invalid_argument("Gender must be either 'male' or 'female'");
+            }
+        }
+};
