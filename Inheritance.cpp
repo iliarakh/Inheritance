31635357d423b9be466879c6dc4ed71ac20f7130
Ilia Rakhoutine <08rakhoutine@gmail.com>
2024-06-10 13:50:54 -0400

    created getters, setters, and birthday function

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -20,4 +20,17 @@ public:
                 throw invalid_argument("Gender must be either 'male' or 'female'");
             }
         }
+        
+    // Getters
+    int getAge() const { return age; }
+    string getName() const { return name; }
+    string getGender() const { return gender; }
+    double getWeight() const { return weight; }
+
+    // Setters
+    void setName(const string& newName) { name = newName; }
+    void setWeight(double newWeight) { weight = newWeight; }
+
+    // Birthday function
+    void birthday() { age++; }
 };
