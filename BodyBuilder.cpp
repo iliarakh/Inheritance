9b43a87c4774ca354609195d09af01bcb3575944
iliarakh <08rakhoutine@gmail.com>
2024-06-10 17:28:51 -0400

    Delete BodyBuilder.cpp

--- BodyBuilder.cpp
+++
@@ -1,8 +0,0 @@
-#include "BodyBuilder.hpp"
-
-BodyBuilder::BodyBuilder(int age, string name, string gender, double weight)
-    : Person(age, name, gender, weight) {}
-
-void BodyBuilder::cut() { setWeight(getWeight() * 0.97); }
-void BodyBuilder::bulk() { setWeight(getWeight() * 1.03); }
-void BodyBuilder::lift() { setWeight(getWeight() * 1.01); }
